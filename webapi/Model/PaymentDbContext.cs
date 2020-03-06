70cd2160b6301b91df2b7af6c27a1e51275d9a8f
dheeraj <dheeraj@SDD-13>
2020-03-06 14:31:36 +0530

    webapi

---
+++ webapi/Model/PaymentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webapi.Model;
+
+namespace webapi.Model
+{
+    public class PaymentDbContext:DbContext
+    {
+        public PaymentDbContext(DbContextOptions<PaymentDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
